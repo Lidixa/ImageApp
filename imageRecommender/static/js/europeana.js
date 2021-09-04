1cfd5e28db937ac0f4bf062e31b02ae5135488b0
Lidiia <loranz@yanex.ru>
2021-09-04 13:42:26 +0100

    edit

--- imageRecommender/static/js/europeana.js
+++ imageRecommender/static/js/europeana.js
@@ -1,18 +1,16 @@
 var apiCall = 'https://www.europeana.eu/api/v2/search.json?wskey=chametel&query=europeana_collectionName%3A9200434*&start=1&rows=24&profile=rich';
 
 $.getJSON(apiCall, function (json) {
-var totalcount = json.totalResults;
-var counthtml = '<h2>Total results: ' + totalcount + '</h2>';
-$('#content').append(counthtml);
-$.each(json.items, function (i, item) {
-// we can now access object metadata in the object 'item'
-});
-var title = item.title;
-var link = item.guid;
-var thumbnail = item.edmPreview;
-
-var objecthtml = '';
-objecthtml += '<a href="'+link+'" title="'+title+'"><img src="'+thumbnail+'" alt="'+title+'"></a>';
-
-$('#content').append(objecthtml);
+    // here we will do something with the response
+    var totalcount = json.totalResults;
+    var counthtml = '<h2>Total results: ' + totalcount + '</h2>';
+    $('#content').append(counthtml);
+    $.each(json.items, function (i, item) {
+        var title = item.title;
+        var link = item.guid;
+        var thumbnail = item.edmPreview;
+        var objecthtml = '';
+        objecthtml += '<a href="' + link + '" class="thumbnail" title="' + title + '" target="_blank"><img src="' + thumbnail + '" alt="' + title + '"></a>';
+        $('#content').append(objecthtml);
+    });
 });
