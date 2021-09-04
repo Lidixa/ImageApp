1adccbd47824260f11cd942f54b857fa38c82d78
Lidiia <loranz@yanex.ru>
2021-09-04 19:42:40 +0100

    edit

--- imageRecommender/static/js/europeana2.js
+++ imageRecommender/static/js/europeana2.js
@@ -4,13 +4,13 @@ $.getJSON(apiCall, function (json) {
     // here we will do something with the response
     var totalcount = json.totalResults;
     var counthtml = '<h2>Total results: ' + totalcount + '</h2>';
-    $('#content').append(counthtml);
+    $('#europeana').append(counthtml);
     $.each(json.items, function (i, item) {
         var title = item.title;
         var link = item.guid;
         var thumbnail = item.edmPreview;
         var objecthtml = '';
         objecthtml += '<a href="' + link + '" class="thumbnail" title="' + title + '" target="_blank"><img src="' + thumbnail + '" alt="' + title + '"></a>';
-        $('#content').append(objecthtml);
+        $('#europeana').append(objecthtml);
     });
 }); 
